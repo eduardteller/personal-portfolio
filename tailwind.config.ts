bb1a1c55adf58d03c20ee73649bde621a8067c2c
eduardteller <edtell@taltech.ee>
2025-01-28 23:41:05 +0200

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			keyframes: {
+				comet: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(110vw) ' },
+				},
+			},
+			animation: {
+				comet: 'comet 1s linear forwards',
+			},
+			fontFamily: {
+				thunder: ['Thunder', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
