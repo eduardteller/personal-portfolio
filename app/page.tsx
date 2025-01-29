1ee391087c36dabe89eb031ba91b1560955e7bbb
eduardteller <edtell@taltech.ee>
2025-01-29 20:58:23 +0200

    james

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LinkBadge from "@/components/LinkBadge";
 import SkillBadge from "@/components/SkillBadge";
 import StarsBackground from "@/components/StarsBG";
+import { FileUser, Mail } from "lucide-react";
 import Image from "next/image";
 import { inter } from "./layout";
 
@@ -54,13 +55,26 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className="flex basis-2/3 items-center justify-center px-8">
+            <div
+              className={`${inter.className} flex basis-2/3 flex-col justify-center gap-4 px-8`}
+            >
               <p className="prose prose-lg text-zinc-100/80">{`My name is Eduard Teller, and I'm a Fullstack Web Developer from Tallinn, Estonia.
               I'm currently studying IT at TalTech, Estonia's largest tech university.
               
               My journey began in 2020 when I decided to learn web development for a hobby project, and I was hooked immediately. I later pursued IT studies at TalTech, where I gained extensive knowledge about software development. I also developed skills in teamwork, essential algorithms, design patterns, and programming fundamentals.
               
               Since then, I've learned a lot about modern technologies and protocols that power the web, including TypeScript, React, Next.js, Node.js, Express, backend systems like Supabase, to name a few. `}</p>
+              <div className="group relative w-fit cursor-default">
+                <div
+                  className={`animate-gradient absolute inset-0 rounded-md bg-gradient-to-r from-blue-600 via-purple-600 to-red-600`}
+                ></div>
+                <div className="relative flex items-center gap-2 rounded-md border border-zinc-100/80 bg-zinc-900 p-4 leading-none duration-150 group-hover:-translate-x-1 group-hover:-translate-y-1">
+                  <FileUser className="h-6 w-6 text-zinc-100/80" />
+                  <p className="text-base font-bold text-zinc-100/80">
+                    Download Resume
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -125,19 +139,32 @@ export default function Home() {
         </div>
       </section>
       <section className="bg-zinc-900">
-        <div className="mx-auto flex w-full max-w-4xl flex-col items-center gap-4 px-4 py-24">
+        <div className="mx-auto flex w-full max-w-5xl flex-col items-center gap-4 px-4 py-24">
           <h5 className="mx-auto w-fit text-sm font-bold uppercase text-blue-600">
             education
           </h5>
           <h2 className="text-4xl font-bold text-zinc-100/80">I Studied At</h2>
         </div>
       </section>
-      <section className="bg-zinc-900">
-        <div className="mx-auto flex w-full max-w-4xl flex-col items-center gap-4 px-4 py-24">
-          <h5 className="mx-auto w-fit text-sm font-bold uppercase text-blue-600">
-            contact
-          </h5>
-          <h2 className="text-4xl font-bold text-zinc-100/80">{`Let's Connect`}</h2>
+      <section className="relative bg-zinc-900">
+        {/* <div className="absolute right-0 top-0 z-[1] h-full w-full bg-zinc-950/40"></div> */}
+        <div className="z-50 mx-auto flex h-full w-full max-w-7xl flex-col items-center px-4 py-24">
+          <div className="flex w-full gap-4">
+            <div className="flex basis-1/2 flex-col gap-4">
+              <h5 className="text-sm font-bold uppercase text-blue-600">
+                contact
+              </h5>
+              <h2 className="text-4xl font-bold text-zinc-100/80">{`Let's Connect`}</h2>
+              <a
+                href="mailto:hello@eduardteller.me"
+                className="flex w-fit items-center gap-2 rounded-md border border-zinc-100/80 p-3 font-medium text-zinc-100/60"
+              >
+                <Mail className="h-6 w-6" />
+                hello@eduardteller.me
+              </a>
+            </div>
+            <div className="basis-1/2"></div>
+          </div>
         </div>
       </section>
     </main>
--- components/SkillBadge.tsx
+++ components/SkillBadge.tsx
@@ -13,12 +13,12 @@ const SkillBadge = ({ image, alt, title, color }: Props) => {
       <div
         className={` ${color === "blue" ? "bg-blue-600" : color === "red" ? "bg-red-600" : color === "purple" ? "bg-purple-600" : "bg-gradient-to-r from-blue-600 via-purple-600 to-red-600"} animate-gradient absolute inset-0 rounded-lg`}
       ></div>
-      <div className="relative flex flex-col items-center justify-center gap-4 overflow-hidden rounded-lg border border-zinc-100/60 bg-zinc-900 px-2 py-4 leading-none duration-300 group-hover:-translate-x-2 group-hover:-translate-y-2">
+      <div className="relative flex flex-col items-center justify-center gap-4 overflow-hidden rounded-lg border border-zinc-100/60 bg-zinc-900 px-2 py-4 leading-none duration-150 group-hover:-translate-x-1 group-hover:-translate-y-1">
         <Image
           width={48}
           height={48}
           alt={alt}
-          className="h-12 w-12 rounded-md object-contain grayscale duration-300 group-hover:grayscale-0"
+          className="h-12 w-12 rounded-md object-contain grayscale duration-150 group-hover:grayscale-0"
           src={image}
         />
         <p className="overflow-hidden whitespace-nowrap text-xs font-bold uppercase text-zinc-300">
